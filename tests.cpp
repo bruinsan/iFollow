c8646ba7030e0fb1120070996e4acca23da9ab35
Bruno Indrigo dos Santos <bruinsan@gmail.com>
2021-03-29 16:14:51 +0200

    add: gtest framework

---
+++ tests.cpp
@@ -0,0 +1,12 @@
+#include "alarm.hpp"
+#include <gtest/gtest.h>
+
+TEST(DummyGroup, DummyTest )
+{
+    ASSERT_EQ(1,1);
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
